c9004d853855ebb5024d0c34513ef58f381d94d1
DaradMosa <118133163+DaradMosa@users.noreply.github.com>
2025-05-24 00:43:25 +0300

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: '/',
+  base: '/Schedulak',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
